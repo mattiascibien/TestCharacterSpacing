9fa53a2bffb1ee1149ada2db837f514ccd04e110
Mattias Cibien <mattias.cibien@bindingfuture.com>
2023-08-24 10:52:12 +0200

    Add project files.

---
+++ TestCharacterSpacing/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace TestCharacterSpacing;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+        {
+#if __ANDROID__
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+#elif __IOS__
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+        });
+    }
+}
---
+++ TestCharacterSpacing/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace TestCharacterSpacing;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void InputView_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (sender as Entry is { } entry)
+        {
+            var characterSpacing = entry.CharacterSpacing;
+            entry.CharacterSpacing = 0;
+            entry.CharacterSpacing = characterSpacing;
+        }
+    }
+}
+
